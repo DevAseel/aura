81ff643d5529346c7a2a88c2cc02cfa773d9cd15
DevAseel <99526005+DevAseel@users.noreply.github.com>
2023-05-25 11:09:03 +0400

    released 0.0.4

--- validate.ts
+++ validate.ts
@@ -1,12 +1,9 @@
 import { ZodObject } from "zod";
-import { config } from "dotenv";
 
 export function validateEnv<T extends ZodObject<any>>(
   schema: T,
   env: any
 ): void {
-  config();
-
   for (const variableName in schema._def.shape) {
     if (!process.env[variableName]) {
       throw new Error(`Missing required environment variable: ${variableName}`);
